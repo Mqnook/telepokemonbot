94ad3f59f9621e954d07c1f2acb69ababa318dc1
Mqnook <76994353+Mqnook@users.noreply.github.com>
2024-07-23 14:27:47 +0300

    Add files via upload

---
+++ M1L4-main/logic.py
@@ -0,0 +1,86 @@
+from random import randint
+import requests
+from datetime import datetime, timedelta
+
+class Pokemon:
+    pokemons = {}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
+
+        self.pokemon_trainer = pokemon_trainer   
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+        self.hp = randint(100, 200)
+        self.power = randint(10, 20)
+        self.last_feed_time = datetime.now()
+
+        Pokemon.pokemons[pokemon_trainer] = self
+
+
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['other']['home']['front_default'])
+        else:
+            return "https://static.wikia.nocookie.net/pokemon/images/0/0d/025Pikachu.png/revision/latest/scale-to-width-down/1000?cb=20181020165701&path-prefix=ru"
+        
+    def attack(self, enemy):
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}!"
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"   
+        
+    def info(self):
+        return f"""Имя твоего покеомона: {self.name}
+        Cила покемона: {self.power}
+        Здоровье покемона: {self.hp}"""
+
+    # Метод класса для получения картинки покемона
+    def show_img(self):
+        return self.img
+    
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {self.last_feed_time+delta_time}" 
+    
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power}"
+    
+    def feed(self):
+        return super().feed(hp_increase = 20)
+
+
+
+class Wizard(Pokemon):
+    def feed(self):
+        return super().feed(feed_interval = 10)
+
+
+
